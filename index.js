ded451528ecbf74e273685739035f06a26105a3d
Paul Lettington <paul@plett.co.uk>
2022-01-09 23:24:41 +0000

    Try and make an iCal

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import * as ICAL from 'ical.js'
+
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
@@ -16,7 +18,17 @@ async function handleRequest(request) {
 
   const data = await resp.json()
 
-  return new Response(JSON.stringify(data), {
-    headers: { 'content-type': 'application/json' },
+  var cal = new ICAL.Component(['vcalendar', [], []]);
+
+  for (let i in data.results) {
+    var vevent = new ICAL.Component('vevent');
+    var e = new ICAL.Event(vevent);
+    e.summary = data.results[i].properties.Name.title[0].plain_text
+
+    cal.addSubcomponent(vevent);
+  }
+
+  return new Response(cal.toString(), {
+    headers: { 'content-type': 'text/plain' },
   })
 }
